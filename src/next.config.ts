106532caff372424860049728a6bb5c836d81be9
Juan Carlos Ureña Clase <evvotest1@gmail.com>
2026-01-12 14:58:35 +0000

    1/1
    Next.js 15.3.8 (outdated)
    Turbopack
    Runtime Error
    
    Error: Invalid s

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,6 +2,14 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
